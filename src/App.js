c37110ce2ecf1fc8b899706edfbba476a4745eb1
nurulamin092 <mnurulamin092@gmial.com>
2022-05-25 11:19:21 +0600

    sign up with email and password

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from './Pages/About/About';
 import SignIn from './Pages/Login/SignIn';
 import Footer from './Pages/Shared/Footer';
 import RequireAuth from './Pages/Login/RequireAuth';
+import SignUp from './Pages/Login/SignUp';
 function App() {
 
   return (
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<RequireAuth><About /></RequireAuth>} />
+        <Route path='signup' element={<SignUp />} />
         <Route path="signIn" element={<SignIn />} />
       </Routes>
       <Footer></Footer>
