3d785047b6a442452a4d2d4f304cf195c2d997ce
nurulamin092 <mnurulamin092@gmial.com>
2022-05-26 09:15:16 +0600

    added product json file

--- src/Pages/Shared/Loading.js
+++ src/Pages/Shared/Loading.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Loading = () => {
 
     return (
-        <button className="btn btn-square loading"></button>
+        <div className="h-screen flex justify-center items-center">
+            <div className="w-16 h-16 border-b-2 border-gray-900 rounded-full animate-spin"></div>
+        </div>
     );
 };
 
