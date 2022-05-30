6cb585e9bc61f0798e15616741155e774bb13b00
nurulamin092 <mnurulamin092@gmial.com>
2022-05-30 10:06:55 +0600

    load order data in payment component

--- src/Pages/Dashboard/Payment.js
+++ src/Pages/Dashboard/Payment.js
@@ -1,22 +1,34 @@
 import React from 'react';
+import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
+import Loading from '../Shared/Loading';
 
 const Payment = () => {
     const { id } = useParams();
+    const url = `http://localhost:5000/order/${id}`;
+    const { data: order, isLoading } = useQuery(['order', id], () => fetch(url, {
+        method: 'GET',
+        headers: {
+            'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res => res.json()));
+
+    if (isLoading) {
+        return <Loading></Loading>
+    }
     return (
         <div>
             <div class="card w-50 max-w-md bg-base-100 shadow-xl my-12">
                 <div class="card-body">
-                    <p className="text-success font-bold">Hello,</p>
-                    <h2 class="card-title">Please Pay for {id} </h2>
-                    <p>Your Appointment: <span className='text-orange-700'></span></p>
-                    <p>Please pay: </p>
+                    <p className="text-success font-bold">Hello {order.customerName}</p>
+                    <h2 class="card-title">Please Pay for {order.purchase}</h2>
+                    <p>Please pay: ${order.price} </p>
                 </div>
             </div>
             <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
                 <div class="card-body">
                     {/* <Elements stripe={stripePromise}>
-                        <CheckoutForm appointment={appointment} />
+                        <CheckoutForm order={order} />
                     </Elements> */}
                 </div>
             </div>
